af8c8591d5387d290a4edb92a7d287be6d38ac23
Benson <dev@thebigflavour.com>
2026-06-09 23:25:53 +0100

    chore: add barrel exports for brand, typography, and media components

---
+++ src/components/brand/index.ts
@@ -0,0 +1,4 @@
+export { BrandLogo } from "./BrandLogo";
+export { BrandButton } from "./BrandButton";
+export { BrandPattern } from "./BrandPattern";
+export { MarqueeLockup } from "./MarqueeLockup";
